94f2d40416b10293bce4beec2072487096714702
Joshua Clemons <joshua.clemons@bison.howard.edu>
2022-12-13 00:47:17 +0000

    first commit

--- shm_processes.c
+++ shm_processes.c
@@ -6,28 +6,26 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-void  ClientProcess(int [], int []);
+void  ClientProcess(int []);
 
-int  main(int  argc, char *argv[]){
+int  main(int  argc, char *argv[])
+{
      int    ShmID;
      int    *ShmPTR;
      pid_t  pid;
      int    status;
-     int fd[2];
-     int buffer[10];
-     pipe(fd);
 
-     if (argc != 11) {
-          printf("Use: %s #1 #2 #3 #4 #5 #6 #7 #8 #9 #10\n", argv[0]);
+     if (argc != 5) {
+          printf("Use: %s #1 #2 #3 #4\n", argv[0]);
           exit(1);
      }
 
-     ShmID = shmget(IPC_PRIVATE, 10*sizeof(int), IPC_CREAT | 0666);
+     ShmID = shmget(IPC_PRIVATE, 4*sizeof(int), IPC_CREAT | 0666);
      if (ShmID < 0) {
           printf("*** shmget error (server) ***\n");
           exit(1);
      }
-     printf("Server has received a shared memory of ten integers...\n");
+     printf("Server has received a shared memory of four integers...\n");
 
      ShmPTR = (int *) shmat(ShmID, NULL, 0);
      if (*ShmPTR == -1) {
@@ -35,10 +33,14 @@ int  main(int  argc, char *argv[]){
           exit(1);
      }
      printf("Server has attached the shared memory...\n");
-    int i;
-    for(i = 0; i < 10; i++){
-      ShmPTR[i] = atoi(argv[i+1]);
-    }
+
+     ShmPTR[0] = atoi(argv[1]);
+     ShmPTR[1] = atoi(argv[2]);
+     ShmPTR[2] = atoi(argv[3]);
+     ShmPTR[3] = atoi(argv[4]);
+     printf("Server has filled %d %d %d %d in shared memory...\n",
+            ShmPTR[0], ShmPTR[1], ShmPTR[2], ShmPTR[3]);
+
      printf("Server is about to fork a child process...\n");
      pid = fork();
      if (pid < 0) {
@@ -46,20 +48,12 @@ int  main(int  argc, char *argv[]){
           exit(1);
      }
      else if (pid == 0) {
-       printf("Child: ");
-       ClientProcess(ShmPTR, fd);
-       exit(0);
+          ClientProcess(ShmPTR);
+          exit(0);
      }
 
      wait(&status);
      printf("Server has detected the completion of its child...\n");
-     close(fd[1]);
-     printf("Parent: ");
-     for(i = 0; i<10; i++){
-       read(fd[0], buffer, sizeof(int));
-       printf("%d  ", *buffer);
-     }
-     printf("\n");
      shmdt((void *) ShmPTR);
      printf("Server has detached its shared memory...\n");
      shmctl(ShmID, IPC_RMID, NULL);
@@ -68,20 +62,10 @@ int  main(int  argc, char *argv[]){
      exit(0);
 }
 
-void  ClientProcess(int  SharedMem[], int fd[]){
-     printf("   Child process started\n");
-     printf("Child: ");
-     int j;
-     for(j=0; j<10; j++){
-       printf("%d  ",SharedMem[j]);
-     }
-     printf("\n");
-     close(fd[0]);
-     int i;
-     for(i=0; i<10; i++){
-       write(fd[1], &SharedMem[i], sizeof(int));
-     }
-     printf("Done Writing to pipe\n");
-     printf("Client is about to exit\n");
-     printf("******************************\n");
+void  ClientProcess(int  SharedMem[])
+{
+     printf("   Client process started\n");
+     printf("   Client found %d %d %d %d in shared memory\n",
+                SharedMem[0], SharedMem[1], SharedMem[2], SharedMem[3]);
+     printf("   Client is about to exit\n");
 }
